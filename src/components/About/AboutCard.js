632e8dffb6b811333bf93e3d12ae6d92992cbcbd
Bravo lu <bravolu@Bravos-MacBook-Pro.local>
2023-06-13 16:09:03 +0800

    feat: add resource

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,33 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+            Hi, I am <span className="purple">LU, Shaohao </span>
+            from <span className="purple"> Zhuhai, Guangdong, China.</span> I am a both software engineer and
+            algorithm engineer.
+            <br /> I got both the bachelor and the master degree in <span className="purple">Sun-Yat-Sen University</span>
+            I have worked in <span className="purple">Tencent Inc.</span> for two years.
             <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            Additionally, I am looking for some opportunities in Hong Kong recently.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            In my spare time, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Workout
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Cooking
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Investing yourself is one of the best investment!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">LU, Shaohao</footer>
         </blockquote>
       </Card.Body>
     </Card>
